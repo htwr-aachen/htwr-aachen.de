ff631c4cdd65ea811f4f5ad13ab353be4a5b9dd7
Jonas Schneider <jonas.max.schneider@gmail.com>
2023-02-12 12:24:38 +0100

    feat: kontrollflussgraphen

--- src/components/spoiler.tsx
+++ src/components/spoiler.tsx
@@ -4,23 +4,27 @@ import { useState } from "react";
 type SpoilerProps = {
   children: React.ReactNode;
   title?: string;
+  blur?: number;
 };
 
-const Spoiler: FC<SpoilerProps> = ({ children }) => {
+const Spoiler: FC<SpoilerProps> = ({ children, blur = 4 }) => {
   const [isSpoilerVisible, setSpoilerVisible] = useState(false);
 
   return (
-    <button
-      type="button"
-      className={`spoiler transition-all ${
-        isSpoilerVisible
-          ? "cursor-text select-text blur-0"
-          : "blur-sm hover:scale-105"
-      }`}
-      onClick={() => setSpoilerVisible(true)}
-    >
-      {children}
-    </button>
+    <div className="w-full">
+      <button
+        type="button"
+        className={`spoiler w-full transition-all ${
+          isSpoilerVisible
+            ? "cursor-text select-text blur-0"
+            : "hover:scale-105"
+        }`}
+        style={{ filter: isSpoilerVisible ? "blur(0)" : `blur(${blur}px)` }}
+        onClick={() => setSpoilerVisible(true)}
+      >
+        {children}
+      </button>
+    </div>
   );
 };
 
