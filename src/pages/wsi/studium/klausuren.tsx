dd8a1659403a7e00720728bda47afeb5df08d512
Jonas Schneider <jonas.max.schneider@gmail.com>
2023-07-15 23:23:01 +0200

    feat: stocha klausuren

--- src/pages/wsi/studium/klausuren.tsx
+++ src/pages/wsi/studium/klausuren.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { join } from "path";
 import type { FC } from "react";
 
 import { HeadLine } from "@/components/rwth/headline";
@@ -7,36 +6,18 @@ import { WSIConfig } from "@/layouts/configs";
 import { Meta } from "@/layouts/Meta";
 import { WSINavbar } from "@/layouts/rwth/instituteConfig";
 import { Main } from "@/layouts/rwth/Main";
-import { getAllDocsFromDir } from "@/lib/documents";
+import { getProtectedDownloads } from "@/lib/documents";
 
 import { MaterialienSchnellzugriff } from "../studium";
 
-const klausurPath = join(
-  process.cwd(),
-  "public",
-  "teaching-assets",
-  "wsi",
-  "klausuren"
-);
-
-type Document = {
-  name: string;
-  url: string;
-  year: number;
-};
-
 type KlausurenProps = {
-  klausuren: Document[];
+  klausuren: string[];
 };
 
 export async function getStaticProps() {
   return {
     props: {
-      klausuren: await getAllDocsFromDir(
-        klausurPath,
-        "/teaching-assets/wsi/klausuren",
-        true
-      ),
+      klausuren: await getProtectedDownloads("stocha"),
     },
   };
 }
@@ -58,25 +39,25 @@ const Klausuren: FC<KlausurenProps> = ({ klausuren }) => {
         <div className="grid grid-rows-2 lg:grid-cols-[1fr_250px] lg:grid-rows-1">
           <div className="mx-2 lg:m-0">
             <span>
-              Wenn jemand weitere hat:{" "}
+              Es gibt fast keine Klausuren für Stocha. Wenn jemand weitere hat:{" "}
               <a href="mailto:jonas.max.schneider@gmail.com">
                 jonas.max.schneider@gmail.com
               </a>
+              . Ich werde sie dann natürlich anonym hier hochladen.
             </span>
-
-            <br />
-
-            <p>
-              Da Klausuren immer problematisch zu verteilen sind werde ich 100%
-              sichere Captchas einbauen. Dauert aber noch kurz.
-            </p>
 
             <ul className="ml-8 mt-8 list-disc">
               {klausuren.map((klausur) => {
                 return (
-                  <li key={klausur.name}>
-                    <Link href={klausur.url} target={"_blank"}>
-                      {klausur.name}
+                  <li key={klausur}>
+                    <Link
+                      href={{
+                        pathname: "/proc-download",
+                        query: { file: klausur },
+                      }}
+                      target="_blank"
+                    >
+                      {klausur}
                     </Link>
                   </li>
                 );
