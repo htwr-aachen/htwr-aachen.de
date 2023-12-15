77876af6616e89618ae11ed8273454043aef7315
Jonas Schneider <jonas.max.schneider@gmail.com>
2023-12-15 02:04:45 +0100

    fix: Footer.tsx

--- src/layouts/rwth/Footer.tsx
+++ src/layouts/rwth/Footer.tsx
@@ -13,7 +13,7 @@ type FooterProps = {
 };
 
 const Footer: FC<FooterProps> = ({ institute }) => {
-  const { _, setShow } = useContext(PushNotifyContext);
+  const { setShow } = useContext(PushNotifyContext);
   return (
     <footer className="inset-x-0 mx-auto h-[220px] w-full border-t-1 bg-[#333] py-12 text-white lg:max-w-[1280px]">
       <InstituteSwitches institute={institute} />
