d799c715fe8f95053d4460db65e0fe55a33cb06d
Jonas Schneider <jonas.max.schneider@gmail.com>
2024-02-03 19:25:08 +0100

    added vercels SpeedInsights & upgrade deps

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Inter, Roboto } from "next/font/google";
 import type { FC } from "react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import AppMain from "@/layouts/AppMain";
 import { AppConfig, BaseURL } from "@/utils/AppConfig";
@@ -98,6 +99,7 @@ const RootLayout: FC<RootLayoutProps> = ({ children }) => {
         />
       </head>
       <body>
+        <SpeedInsights></SpeedInsights>
         <div className={`${inter.className} ${roboto.className}`}>
           <div className="bg"></div>
           <AppMain>{children}</AppMain>
