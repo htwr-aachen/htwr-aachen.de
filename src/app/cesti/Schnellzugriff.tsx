1b076f3b343f2ec6c5c8997b66b29d9219d58537
Jonas Schneider <jonas.max.schneider@gmail.com>
2024-02-08 18:19:18 +0100

    feat: add itsec material (#31)

---
+++ src/app/cesti/Schnellzugriff.tsx
@@ -0,0 +1,21 @@
+import type { FC } from "react";
+
+import {
+  Schnellzugriff,
+  Schnellzugrifflink,
+} from "@/components/rwth/schnellzugriff";
+
+export const MaterialienSchnellzugriff: FC = () => {
+  return (
+    <div className="max-h-[410]">
+      <Schnellzugriff title="Materialien">
+        <Schnellzugrifflink href="/cesti/studium//teachings">
+          Zusammenfassungen
+        </Schnellzugrifflink>
+        <Schnellzugrifflink href="/cesti/studium/material">
+          Material oder so
+        </Schnellzugrifflink>
+      </Schnellzugriff>
+    </div>
+  );
+};
--- src/app/cesti/page.tsx
+++ src/app/cesti/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 
 import { HeadLine } from "@/components/rwth/headline";
+import { MaterialienSchnellzugriff } from "./Schnellzugriff";
 
 export const metadata: Metadata = {
   description: "The chair for security or it but not both",
@@ -13,6 +14,7 @@ export default function Page() {
   return (
     <div>
       <HeadLine>Wird gerade vorbereitet...</HeadLine>
+      <MaterialienSchnellzugriff />
     </div>
   );
 }
