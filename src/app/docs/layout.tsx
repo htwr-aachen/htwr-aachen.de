5838e88a3ce5e4d61186f499c96101651ace3980
Jonas Schneider <jonas.max.schneider@gmail.com>
2024-03-15 21:15:42 +0100

    feat: include lucide/feather icons

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,11 +1,5 @@
 import "../../styles/simple.scss";
 
-import Main from "@/layouts/Main";
-
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <Main institute="htwr">
-      <div>{children}</div>
-    </Main>
-  );
+  return { children };
 }
