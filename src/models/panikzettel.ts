ab3bf86ddfdb5638ebc86dbf3196498ac458ade0
Jonas Schneider <jonas.max.schneider@gmail.com>
2024-08-04 22:49:56 +0200

    fix: rename panikzettel subject type names & sort panikzettel

--- src/models/panikzettel.ts
+++ src/models/panikzettel.ts
@@ -1,9 +1,14 @@
 export type Panikzettel = {
-  name: string;
-  type: "pf" | "wpf" | "af";
-  filename: string;
-  url: string;
-  semester?: number;
-  shortname?: string;
-  date?: string;
+	name: string;
+	type: "pf" | "wpf" | "af";
+	filename: string;
+	url: string;
+	semester?: number;
+	shortname?: string;
+	date?: string;
 };
+
+export const COMPULSORY_SUBJECT = "compulsory";
+export const COMPULSORY_ELECTIVE_SUBJECT = "compulsory_elective";
+export const APPLICATION_AREA_SUBJECT = "application_area";
+export const NON_TECHNICAL_ELECTIVE_SUBJECT = "non_technical_elective";
