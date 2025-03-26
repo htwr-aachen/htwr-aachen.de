14e643376543e0e0dd4090954971a534f80adb92
Jonas Schneider <jonas.max.schneider@gmail.com>
2025-03-26 18:02:52 +0100

    fix: prevent additional params to div in summary layout

--- src/components/summaries/layout.tsx
+++ src/components/summaries/layout.tsx
@@ -14,46 +14,46 @@ export default function SummaryLayout(
     prev?: ArticleMeta;
   } & HTMLAttributes<HTMLDivElement>,
 ) {
+  const { children, meta, subjectConfig, next, prev, ...divProps} = props
   return (
     <article className="markdown line-numbers">
       <script
         type="application/ld+json"
         dangerouslySetInnerHTML={{
-          __html: JSON.stringify(getJSONLD(props.meta)),
+          __html: JSON.stringify(getJSONLD(meta)),
         }}
       />
       <div>
-        <h1 className="inline text-2xl font-bold">{props.meta.meta.title}</h1> |{" "}
-        <Link href={props.subjectConfig.articlesURL}>Back to Overview</Link>
+        <h1 className="inline text-2xl font-bold">{meta.meta.title}</h1> |{" "}
+        <Link href={subjectConfig.articlesURL}>Back to Overview</Link>
       </div>
       <div
-        className={`wrapper m-4 border-1 border-gray-600 p-4 pb-8 lg:px-8 ${props.className}`}
-        /* @next-codemod-error 'props' is used with spread syntax (...). Any asynchronous properties of 'props' must be awaited when accessed. */
-        {...props}
+        className={`wrapper m-4 border-1 border-gray-600 p-4 pb-8 lg:px-8 ${divProps.className}`}
+        {...divProps}
       >
-        {props.children}
+        {children}
       </div>
       <div>
-        <Link href={props.subjectConfig.articlesURL}>Back to Overview</Link>{" "}
-        {props.prev != null ? (
+        <Link href={subjectConfig.articlesURL}>Back to Overview</Link>{" "}
+        {prev != null ? (
           <>
             | Vorheriges:{" "}
             <Link
-              href={`${props.subjectConfig.articlesURL}/${props.prev?.slug}`}
+              href={`${subjectConfig.articlesURL}/${prev?.slug}`}
             >
-              {props.prev?.meta.title}
+              {prev?.meta.title}
             </Link>{" "}
           </>
         ) : (
           <></>
         )}
-        {props.next != null ? (
+        {next != null ? (
           <>
             | Nächstes:{" "}
             <Link
-              href={`${props.subjectConfig.articlesURL}/${props.next?.slug}`}
+              href={`${subjectConfig.articlesURL}/${next?.slug}`}
             >
-              {props.next?.meta.title}
+              {next?.meta.title}
             </Link>{" "}
           </>
         ) : (
