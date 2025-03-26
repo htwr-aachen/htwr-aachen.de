0f998fe444d5cab3e5a94b09e77b720984e01a7f
Jonas Schneider <jonas.max.schneider@gmail.com>
2025-03-26 18:01:10 +0100

    fix: use loopback ip instead of localhost as BASE_URL

--- src/config/app.ts
+++ src/config/app.ts
@@ -8,7 +8,7 @@ export const AppConfig = {
 export const BaseURL =
   process.env.NODE_ENV === "production"
     ? "https://htwr-aachen.de"
-    : "http://localhost:3000";
+    : "http://127.0.0.1:3000";
 
 // Uncomment on local api integration testing
 // export const APIURL =
