03255ada5173262be321eb30d81bbd79692b627d
Jonas Schneider <jonas.max.schneider@gmail.com>
2025-05-09 11:34:41 +0200

    add blog about syscom & rwth.cool & add Banner

--- src/layouts/syscom/layout.tsx
+++ src/layouts/syscom/layout.tsx
@@ -3,6 +3,8 @@ import { SysComNav } from "./nav";
 import { NavbarConfig } from "@/models/layout";
 import { Institutes } from "@/config/institutes";
 import { SysComFooter } from "./footer";
+import { BannerContent } from "@/components/banner-provider";
+import { BlogBannerContent } from "@/components/banner";
 
 const SysComLayout = (props: {
   children: ReactNode;
@@ -11,8 +13,11 @@ const SysComLayout = (props: {
   nav: NavbarConfig;
 }) => {
   return (
-    <div className="bg-rwth-branding light min-h-screen">
+    <div className="bg-rwth-branding light min-h-screen text-black">
       <SysComNav config={props.nav} />
+      <BannerContent>
+        <BlogBannerContent />
+      </BannerContent>
       <div className="pt-40 md:pt-24 lg:min-h-[calc(100vh-310px)]">
         {props.children}
       </div>
