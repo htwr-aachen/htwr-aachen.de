a858e68c23b3f1bf3fe07b1c85c0723d0a86a625
Jonas Schneider <jonas.max.schneider@gmail.com>
2025-07-19 23:12:22 +0200

    fix: bg of mobile quick access

--- src/components/faculties-nav/mobile.tsx
+++ src/components/faculties-nav/mobile.tsx
@@ -14,81 +14,77 @@ export function FacultiesMobileNav() {
     Object.values(RealInstituteConfig),
   );
   return (
-    <div className="bg-rwth-accent h-full p-0 lg:hidden">
-      <div className="relative">
-        <div className="grid grid-cols-[auto_1fr] items-center">
-          <div className="border-y-1 border-dotted border-white/10 p-3">
-            <SheetClose className="rounded bg-white/10 px-2 py-1 text-sm hover:bg-black/100">
-              Zurück
-            </SheetClose>
-          </div>
-          <div className="w-full px-3 py-2">
-            <input
-              className="w-full rounded bg-white/10 px-2 py-1 text-sm"
-              placeholder="Search"
-              onInput={(e) => {
-                searchInstitutes(e.currentTarget.value);
-              }}
-            ></input>
-          </div>
+    <div className="bg-cms-branding text-cms-branding-text p-0 lg:hidden">
+      <div className="grid grid-cols-[auto_1fr] items-center">
+        <div className="border-y-1 border-dotted border-white/10 p-3">
+          <SheetClose className="rounded bg-white/10 px-2 py-1 text-sm hover:bg-black/100">
+            Zurück
+          </SheetClose>
         </div>
-        <div className="border-r-1 border-white/10">
-          <FacultiesNavHeading>
-            {FacultiesNavContent.left.heading}
-          </FacultiesNavHeading>
-          <ul className="m-0 w-full">
-            {FacultiesNavContent.left.links.map((link) => (
-              <FacultiesNavLink
-                href={link.href}
-                tooltipContent={link.tooltip || ""}
-                tooltipPlace="right"
-                key={link.name}
-                subElement={link.subcontent}
-              >
-                {link.content}
-              </FacultiesNavLink>
-            ))}
-          </ul>
+        <div className="w-full px-3 py-2">
+          <input
+            className="w-full rounded bg-white/10 px-2 py-1 text-sm"
+            placeholder="Search"
+            onInput={(e) => {
+              searchInstitutes(e.currentTarget.value);
+            }}
+          ></input>
         </div>
-        <div className="border-l-1 border-white/10">
-          <FacultiesNavHeading>
-            {FacultiesNavContent.right.heading}
-          </FacultiesNavHeading>
-          <ul className="m-0 w-full">
-            {FacultiesNavContent.right.links.map((link) => (
-              <FacultiesNavLink
-                href={link.href}
-                tooltipContent={link.tooltip || ""}
-                tooltipPlace="right"
-                key={link.name}
-                subElement={link.subcontent}
-              >
-                {link.content}
-              </FacultiesNavLink>
-            ))}
-          </ul>
-        </div>
+      </div>
+      <div className="border-r-1 border-white/10">
+        <FacultiesNavHeading>
+          {FacultiesNavContent.left.heading}
+        </FacultiesNavHeading>
+        <ul className="m-0 w-full">
+          {FacultiesNavContent.left.links.map((link) => (
+            <FacultiesNavLink
+              href={link.href}
+              tooltipContent={link.tooltip || ""}
+              tooltipPlace="right"
+              key={link.name}
+              subElement={link.subcontent}
+            >
+              {link.content}
+            </FacultiesNavLink>
+          ))}
+        </ul>
+      </div>
+      <div className="border-l-1 border-white/10">
+        <FacultiesNavHeading>
+          {FacultiesNavContent.right.heading}
+        </FacultiesNavHeading>
+        <ul className="m-0 w-full">
+          {FacultiesNavContent.right.links.map((link) => (
+            <FacultiesNavLink
+              href={link.href}
+              tooltipContent={link.tooltip || ""}
+              tooltipPlace="right"
+              key={link.name}
+              subElement={link.subcontent}
+            >
+              {link.content}
+            </FacultiesNavLink>
+          ))}
+        </ul>
+      </div>
+      <div>
         <div>
-          <div>
-            <FacultiesNavHeading>Lehrstühle und Institute</FacultiesNavHeading>
-            <div>
-              <ul className="m-0 w-full">
-                {filteredInstitute.map((institute) => {
-                  return (
-                    <FacultiesNavLink
-                      key={institute.name}
-                      href={institute.href}
-                      subElement={findAssociatedSubjects(institute.name).join(
-                        " | ",
-                      )}
-                      tooltipContent={institute.fullName}
-                      tooltipPlace="left"
-                    >{`${institute.name} (${institute.professor})`}</FacultiesNavLink>
-                  );
-                })}
-              </ul>
-            </div>
-          </div>
+          <FacultiesNavHeading>Lehrstühle und Institute</FacultiesNavHeading>
+          <ul className="m-0 w-full">
+            {filteredInstitute.map((institute) => {
+              return (
+                <FacultiesNavLink
+                  key={institute.name}
+                  href={institute.href}
+                  subElement={findAssociatedSubjects(institute.name).join(
+                    " | ",
+                  )}
+                  tooltipContent={institute.fullName}
+                  tooltipPlace="left"
+                >{`${institute.name} (${institute.professor})`}</FacultiesNavLink>
+              );
+            })}
+          </ul>
         </div>
       </div>
     </div>
